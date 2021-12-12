d05c3c9a65344b29840bd52dab2aaa3c0d6cdaf1
Cleyson Lima <cleysonph@gmail.com>
2021-12-11 21:19:37 -0300

    initialize project

---
+++ src/test/java/br/com/treinaweb/personalexpensesmanager/PersonalExpensesManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.treinaweb.personalexpensesmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalExpensesManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
