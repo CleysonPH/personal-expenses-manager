5ede9ef6580ad756afe181111577e73a0ba62494
Cleyson Lima <cleysonph@gmail.com>
2021-12-12 19:55:30 -0300

    document create account route

--- src/main/java/br/com/treinaweb/personalexpensesmanager/api/v1/controllers/AccountController.java
+++ src/main/java/br/com/treinaweb/personalexpensesmanager/api/v1/controllers/AccountController.java
@@ -6,9 +6,22 @@ import org.springframework.http.ResponseEntity;
 
 import br.com.treinaweb.personalexpensesmanager.api.v1.dtos.requests.AccountRequest;
 import br.com.treinaweb.personalexpensesmanager.api.v1.dtos.responses.AccountResponse;
+import br.com.treinaweb.personalexpensesmanager.api.v1.dtos.responses.ErrorResponse;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
+@Api(tags = "Accounts", description = "Account Controller")
 public interface AccountController {
 
+    @ApiOperation("Cadastrar nova conta")
+    @ApiResponses({
+        @ApiResponse(responseCode = "201", description = "Conta foi criada com sucesso"),
+        @ApiResponse(responseCode = "400", description = "Houveram erros de validação", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+    })
     AccountResponse create(AccountRequest request);
 
     List<AccountResponse> findAll();
