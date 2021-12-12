983a24da8628157091ea4143192cd5a8f2c11b4d
Cleyson Lima <cleysonph@gmail.com>
2021-12-11 22:43:27 -0300

    implements create method in AccountService

--- src/main/java/br/com/treinaweb/personalexpensesmanager/api/v1/services/AccountService.java
+++ src/main/java/br/com/treinaweb/personalexpensesmanager/api/v1/services/AccountService.java
@@ -1,5 +1,10 @@
 package br.com.treinaweb.personalexpensesmanager.api.v1.services;
 
+import br.com.treinaweb.personalexpensesmanager.api.v1.dtos.requests.AccountRequest;
+import br.com.treinaweb.personalexpensesmanager.api.v1.dtos.responses.AccountResponse;
+
 public interface AccountService {
+
+    AccountResponse create(AccountRequest request);
 
 }
