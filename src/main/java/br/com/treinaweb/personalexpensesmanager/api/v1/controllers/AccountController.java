ff732c0f906f71f2c7c980ff87b7694b24c02618
Cleyson Lima <cleysonph@gmail.com>
2021-12-12 20:21:34 -0300

    resolve code smells in account controller documentation

--- src/main/java/br/com/treinaweb/personalexpensesmanager/api/v1/controllers/AccountController.java
+++ src/main/java/br/com/treinaweb/personalexpensesmanager/api/v1/controllers/AccountController.java
@@ -7,49 +7,39 @@ import org.springframework.http.ResponseEntity;
 import br.com.treinaweb.personalexpensesmanager.api.v1.dtos.requests.AccountRequest;
 import br.com.treinaweb.personalexpensesmanager.api.v1.dtos.responses.AccountResponse;
 import br.com.treinaweb.personalexpensesmanager.api.v1.dtos.responses.ErrorResponse;
+import br.com.treinaweb.personalexpensesmanager.core.config.SwaggerConfig;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
-@Api(tags = "Accounts", description = "Account Controller")
+@Api(tags = SwaggerConfig.ACCOUNTS_TAG)
 public interface AccountController {
 
     @ApiOperation("Cadastrar nova conta")
-    @ApiResponses({
-        @ApiResponse(responseCode = "201", description = "Conta foi criada com sucesso"),
-        @ApiResponse(responseCode = "400", description = "Houveram erros de validação", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
-    })
+    @ApiResponse(responseCode = "201", description = "Conta foi criada com sucesso")
+    @ApiResponse(responseCode = "400", description = "Houveram erros de validação", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
     AccountResponse create(AccountRequest request);
 
     @ApiOperation("Lista todas as contas")
-    @ApiResponses({
-        @ApiResponse(responseCode = "200", description = "Listagem realizada com sucesso")
-    })
+    @ApiResponse(responseCode = "200", description = "Listagem realizada com sucesso")
     List<AccountResponse> findAll();
 
     @ApiOperation("Busca conta por id")
-    @ApiResponses({
-        @ApiResponse(responseCode = "200", description = "Conta encontrada com sucesso"),
-        @ApiResponse(responseCode = "404", description = "Conta não encontrada", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
-    })
+    @ApiResponse(responseCode = "200", description = "Conta encontrada com sucesso")
+    @ApiResponse(responseCode = "404", description = "Conta não encontrada", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
     AccountResponse findById(Long accountId);
 
     @ApiOperation("Excluir conta por id")
-    @ApiResponses({
-        @ApiResponse(responseCode = "204", description = "Conta encontrada com sucesso"),
-        @ApiResponse(responseCode = "404", description = "Conta não encontrada", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
-    })
+    @ApiResponse(responseCode = "204", description = "Conta encontrada com sucesso")
+    @ApiResponse(responseCode = "404", description = "Conta não encontrada", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
     ResponseEntity<Void> deleteById(Long accountId);
 
     @ApiOperation("Atualizar conta por id")
-    @ApiResponses({
-        @ApiResponse(responseCode = "200", description = "Conta atualizada com sucesso"),
-        @ApiResponse(responseCode = "400", description = "Houveram erros de validação", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
-        @ApiResponse(responseCode = "404", description = "Conta não encontrada", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
-    })
+    @ApiResponse(responseCode = "200", description = "Conta atualizada com sucesso")
+    @ApiResponse(responseCode = "400", description = "Houveram erros de validação", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+    @ApiResponse(responseCode = "404", description = "Conta não encontrada", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
     AccountResponse updateById(AccountRequest request, Long accountId);
 
 }
--- src/main/java/br/com/treinaweb/personalexpensesmanager/core/config/SwaggerConfig.java
+++ src/main/java/br/com/treinaweb/personalexpensesmanager/core/config/SwaggerConfig.java
@@ -11,6 +11,7 @@ import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.Tag;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -29,6 +30,8 @@ public class SwaggerConfig {
 
     private final TypeResolver typeResolver;
 
+    public static final String ACCOUNTS_TAG = "Accounts";
+
     public SwaggerConfig(TypeResolver typeResolver) {
         this.typeResolver = typeResolver;
     }
@@ -42,6 +45,7 @@ public class SwaggerConfig {
             .apis(RequestHandlerSelectors.basePackage(BASE_PACKAGE))
             .paths(PathSelectors.any())
             .build()
+            .tags(new Tag(ACCOUNTS_TAG, "Account Controller"))
             .apiInfo(buildApiInfo());
     }
 
